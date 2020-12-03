c36733698e91e0449bfc01fed4ac11704fe691c0
ellurguru <70204948+ellurguru@users.noreply.github.com>
2020-12-03 09:47:00 +0530

    Add files via upload

---
+++ EmployeeJourney.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLK_HUB_WEBAPI.Models
+{
+    public class EmployeeJourney
+    {
+        public string empid { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+        public string video_link { get; set; }
+        public string video_description { get; set; }
+        public List<records> records { get; set; }
+
+    }
+
+    public class records
+    {
+        public bool? highlight { get; set; }
+        public bool? star { get; set; }
+        public string title { get; set; }
+        public string year { get; set; }
+    }
+   }
